f7e56402041b5e973511c799447da0a480e45492
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2022-12-14 15:50:43 -0800

    my error solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - prints a given phrase 
- * Return: 0 if exited properly, non-zero otherwise 
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
  * End
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
